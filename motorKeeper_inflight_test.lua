b6891a1240387b4defa11d2c411295a68b028a88
Thiago Lages <thiagolagesrocha@gmail.com>
2023-01-11 12:13:52 -0300

    Add changes made during flight tests.

--- motorKeeper_inflight_test.lua
+++ motorKeeper_inflight_test.lua
@@ -9,9 +9,10 @@ local motorsPWM = {-1, -1, -1, -1, -1, -1}  -- motorsPWM array
 local motorsChannel = {37, 38, 35, 36, 34, 33}
 
 -- modes and channel numbers
-local MODE_ALT_HOLD         = 2
-local MODE_AUTO             = 3
-local MODE_SMART_RTL        = 21
+local COPTER_MODE_ALT_HOLD  = 2
+local COPTER_MODE_AUTO      = 3
+local COPTER_MODE_LOITER    = 5
+local COPTER_MODE_SMART_RTL = 21
 local CHANNEL_PAYLOAD       = 28
 local PAYLOAD_RELEASE_PWM   = 1600
 
@@ -25,6 +26,9 @@ local startTimeModeAuto                 = -1    -- to control time after enterin
 -- local PWMmax = param:get("MOT_PWM_MAX")
 local PWMmin = 1290 -- minimum value in crash log: 1284
 local PWMmax = 1890 -- maximum value in crash log: 1898
+
+-- check if default parameters are on drone
+local isDefaultParameters = true;
 
 -- parameters that will be changed in case the aircraft loses a motor
 local PARAMETERS_NAMES = {
@@ -43,11 +47,15 @@ local PARAMETERS_NAMES = {
     "WPNAV_SPEED_DN"
 }
 
--- will store parameters initial values. Will be filled by function getInitialParams()
-local PARAMETERS = {}
+-- will store parameters initial values. Will be filled by function getDefaultParams()
+local PARAMETERS_DEFAULT = {}
 
 -- parameters new values, in case aircraft loses a motor. Will be filled by function calculateParamsNewValue()
 local PARAMETERS_NEW_VALUE = {}
+
+local function isEmpty(s)
+    return s == nil or s == ''
+  end
 
 -- get motors updated PWM value
 local function updateMotorsPWM()
@@ -55,7 +63,7 @@ local function updateMotorsPWM()
     for i = 1, numMotors, 1 do
         channel     = motorsChannel[i]
         motorsPWM[i]= SRV_Channels:get_output_pwm(channel)
-        -- gcs:send_text(0, string.format("motor %d = %d", i, motorsPWM[i]))
+        -- gcs:send_text(6, string.format("motor %d = %d", i, motorsPWM[i]))
     end
 end
 
@@ -65,11 +73,11 @@ local function isAnyMotorOutsidePWMRange()
     local motorPWM = -1
 
     for i = 1, numMotors, 1 do
-        -- gcs:send_text(0, string.format("i = %d", i))
+        -- gcs:send_text(6, string.format("i = %d", i))
         motorPWM = motorsPWM[i]
         if (motorPWM ~= nil) then
             if (motorPWM >= PWMmax or motorPWM <= PWMmin) then
-                gcs:send_text(6, string.format("motor %d is NOT OK, PWM = %d", i, motorPWM ))
+                gcs:send_text(6, string.format("[motorKeeper.lua] motor %d is NOT OK, PWM = %d", i, motorPWM ))
                 return true
             end
             -- gcs:send_text(6, string.format("motorPWM %d is OK, value %d", i, motorPWM ))
@@ -78,7 +86,7 @@ local function isAnyMotorOutsidePWMRange()
         end
     end
     -- if nothing else returned true, then we should return false
-    -- gcs:send_text(0, string.format("everything OK, returning false"))
+    -- gcs:send_text(6, string.format("everything OK, returning false"))
     return false
 end
 
@@ -100,11 +108,37 @@ local function isMotorDown()
     return false
 end
 
+-- get initial parameters that will be changed in case the aircraft loses a motor
+local function getDefaultParams()
+    -- fill PARAMETERS_DEFAULT table with initial parameters
+    for i, paramName in ipairs(PARAMETERS_NAMES) do
+        PARAMETERS_DEFAULT[paramName] = param:get(paramName)
+        if (isEmpty(PARAMETERS_DEFAULT[paramName]))then
+            gcs:send_text(6, string.format("[motorKeeper.lua] %s NOT FOUND", paramName))
+        else
+            gcs:send_text(6, string.format("[motorKeeper.lua] %s default value is %f", paramName, PARAMETERS_DEFAULT[paramName]))
+        end
+    end
+end
+
+local function setDefaultParams()
+    for index, paramName in ipairs(PARAMETERS_NAMES) do
+        local result = param:set(paramName, PARAMETERS_DEFAULT[paramName])
+        if (result) then
+            gcs:send_text(6, string.format("[motorKeeper.lua] Changing %s to %f", paramName, PARAMETERS_NEW_VALUE[paramName]))    
+        else
+            gcs:send_text(6, string.format("[motorKeeper.lua] %s NOT FOUND, not setting it.", paramName))
+        end
+    end
+    isDefaultParameters = true
+end
+
 local function setParamsNewValue()
     for index, paramName in ipairs(PARAMETERS_NAMES) do
         param:set(paramName, PARAMETERS_NEW_VALUE[paramName])
         gcs:send_text(0, string.format("[motorKeeper.lua] Changing %s to %f", paramName, PARAMETERS_NEW_VALUE[paramName]))
     end
+    isDefaultParameters = false
 end
 
 -- action to be taken in case the aircraft loses a motor
@@ -114,12 +148,12 @@ local function motorDownAction()
     gcs:send_text(0, "[motorKeeper.lua] Motor Down")
 
     -- change parameters
-    gcs:send_text(0, "[motorKeeper.lua] Changing parameter values")
+    gcs:send_text(6, "[motorKeeper.lua] Changing parameter values")
     setParamsNewValue()
 
     -- change flght mode to smartRTL
-    gcs:send_text(0, "[motorKeeper.lua] Changing mode to SMART_RTL")
-    vehicle:set_mode(MODE_SMART_RTL)
+    -- gcs:send_text(0, "[motorKeeper.lua] Changing mode to SMART_RTL")
+    -- vehicle:set_mode(COPTER_MODE_SMART_RTL)
 
 end
 
@@ -131,46 +165,44 @@ local function motorkeeper()
 
     -- check if any motors have stopped
     local mode = vehicle:get_mode()
-    if(mode == MODE_AUTO and vehicle:is_armed()) then
-        -- gcs:send_text(6, "[motorKeeper.lua] mode AUTO")
-        if (startTimeModeAuto <= 0)then
-            gcs:send_text(6, "[motorKeeper.lua] starting to count startTimeModeAuto")
-            startTimeModeAuto = millis()
-        end
-        local timeSinceAuto = millis() - startTimeModeAuto
-        if (timeSinceAuto >= WAIT_TIME_MODE_AUTO_MS) then
-            if (isMotorDown()) then
-                gcs:send_text(6, "[motorKeeper.lua] inside WAIT_TIME_MODE_AUTO_MS")
-                motorDownAction()
+    if (arming:is_armed()) then
+        if(mode == COPTER_MODE_AUTO) then
+        -- if(mode == COPTER_MODE_LOITER) then
+            -- gcs:send_text(6, "[motorKeeper.lua] mode AUTO")
+            if (startTimeModeAuto <= 0) then
+                gcs:send_text(6, "[motorKeeper.lua] starting to count startTimeModeAuto")
+                startTimeModeAuto = millis()
             end
+            local timeSinceAuto = millis() - startTimeModeAuto
+            if (timeSinceAuto >= WAIT_TIME_MODE_AUTO_MS) then
+                if (isMotorDown()) then
+                    gcs:send_text(6, "[motorKeeper.lua] inside WAIT_TIME_MODE_AUTO_MS")
+                    motorDownAction()
+                end
+            end
+        else
+            startTimeModeAuto = -1
+        end -- auto
+    else -- not armed
+        if (not isDefaultParameters) then
+            setDefaultParams()
         end
-    else
-        startTimeModeAuto = -1
-    end
+    end -- end is_armed
 
     return motorkeeper, 50 -- run every 50ms
-end
-
--- get initial parameters that will be changed in case the aircraft loses a motor
-local function getInitialParams()
-    -- fill PARAMETERS table with initial parameters
-    for i, paramName in ipairs(PARAMETERS_NAMES) do
-        PARAMETERS[paramName] = param:get(paramName)
-        gcs:send_text(0, string.format(" %s initial value is %f", paramName, PARAMETERS[paramName]))
-    end
 end
 
 -- setup the new parameter values to be set if aircraft loses a motor
 local function calculateParamsNewValue()
 
     PARAMETERS_NEW_VALUE["MOT_YAW_HEADROOM"]= 0                                   -- "MOT_YAW_HEADROOM"
-    PARAMETERS_NEW_VALUE["ATC_SLEW_YAW"]    = 0.5 * PARAMETERS["ATC_SLEW_YAW"]    -- "ATC_SLEW_YAW"
-    PARAMETERS_NEW_VALUE["ATC_ACCEL_Y_MAX"] = 0.5 * PARAMETERS["ATC_ACCEL_Y_MAX"] -- "ATC_ACCEL_Y_MAX"
-    PARAMETERS_NEW_VALUE["ATC_ACCEL_R_MAX"] = 0.7 * PARAMETERS["ATC_ACCEL_R_MAX"] -- "ATC_ACCEL_R_MAX"
-    PARAMETERS_NEW_VALUE["ATC_ACCEL_P_MAX"] = 0.7 * PARAMETERS["ATC_ACCEL_P_MAX"] -- "ATC_ACCEL_P_MAX"
-    PARAMETERS_NEW_VALUE["ATC_RATE_R_MAX"]  = 0.7 * PARAMETERS["ATC_RATE_R_MAX"]  -- "ATC_RATE_R_MAX"
-    PARAMETERS_NEW_VALUE["ATC_RATE_P_MAX"]  = 0.7 * PARAMETERS["ATC_RATE_P_MAX"]  -- "ATC_RATE_P_MAX"
-    PARAMETERS_NEW_VALUE["ATC_RATE_Y_MAX"]  = 0.7 * PARAMETERS["ATC_RATE_Y_MAX"]  -- "ATC_RATE_Y_MAX"
+    PARAMETERS_NEW_VALUE["ATC_SLEW_YAW"]    = 0.5 * PARAMETERS_DEFAULT["ATC_SLEW_YAW"]    -- "ATC_SLEW_YAW"
+    PARAMETERS_NEW_VALUE["ATC_ACCEL_Y_MAX"] = 0.5 * PARAMETERS_DEFAULT["ATC_ACCEL_Y_MAX"] -- "ATC_ACCEL_Y_MAX"
+    PARAMETERS_NEW_VALUE["ATC_ACCEL_R_MAX"] = 0.7 * PARAMETERS_DEFAULT["ATC_ACCEL_R_MAX"] -- "ATC_ACCEL_R_MAX"
+    PARAMETERS_NEW_VALUE["ATC_ACCEL_P_MAX"] = 0.7 * PARAMETERS_DEFAULT["ATC_ACCEL_P_MAX"] -- "ATC_ACCEL_P_MAX"
+    PARAMETERS_NEW_VALUE["ATC_RATE_R_MAX"]  = 0.7 * PARAMETERS_DEFAULT["ATC_RATE_R_MAX"]  -- "ATC_RATE_R_MAX"
+    PARAMETERS_NEW_VALUE["ATC_RATE_P_MAX"]  = 0.7 * PARAMETERS_DEFAULT["ATC_RATE_P_MAX"]  -- "ATC_RATE_P_MAX"
+    PARAMETERS_NEW_VALUE["ATC_RATE_Y_MAX"]  = 0.7 * PARAMETERS_DEFAULT["ATC_RATE_Y_MAX"]  -- "ATC_RATE_Y_MAX"
     PARAMETERS_NEW_VALUE["PLND_ENABLED"]    = 0                                   -- "PLND_ENABLED"
     PARAMETERS_NEW_VALUE["WPNAV_SPEED"]     = 1000                                -- "WPNAV_SPEED"
     PARAMETERS_NEW_VALUE["WPNAV_RADIUS"]    = 750                                 -- "WPNAV_RADIUS"
@@ -180,23 +212,24 @@ local function calculateParamsNewValue()
 end
 
 
-local function printParamsNewValue()
+local function printNewParams()
     for i, paramName in ipairs(PARAMETERS_NAMES) do
-        gcs:send_text(0, string.format("%s new value is %f", paramName, PARAMETERS_NEW_VALUE[paramName]))
+        gcs:send_text(6, string.format("[motorKeeper.lua] %s NEW value is %f", paramName, PARAMETERS_NEW_VALUE[paramName]))
     end
 end
 
 -- setup function
 local function setupMotorKeeper()
 
-    getInitialParams()
+    getDefaultParams()
     calculateParamsNewValue()
-    printParamsNewValue()
+    --printParams(PARAMETERS_DEFAULT, "DEFAULT")
+    printNewParams()
     
 end
 
 
-gcs:send_text(0, "motorKeeper.lua is running")
+gcs:send_text(6, "motorKeeper.lua is running")
 setupMotorKeeper() -- setup parameters and everythig else that needs a setup
 
 return motorkeeper(), 5000 -- run 5s after start
