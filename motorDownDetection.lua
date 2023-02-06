4673fa93df296fe8fe1554de24d9a0d3b8351e65
Thiago Lages <thiagolagesrocha@gmail.com>
2023-02-06 17:44:11 -0300

    Add coded tested on the real drone. All were successful.

--- motorDownDetection.lua
+++ motorDownDetection.lua
@@ -1,20 +1,23 @@
--- code by Thiago Lages (github: @thiagolages)
 -- variables
 
 -- run script at this amount of millisecond interval
-local loop_time = 10 -- milliseconds. 1/10ms = 100Hz
+local loop_time     = 10 -- milliseconds. 1/10ms = 100Hz
 
 local numMotors     = 6                         -- total number of motors
 local motorsPWM     = { -1, -1, -1, -1, -1, -1} -- motorsPWM array
-local motorsChannel = {33, 34, 35, 36, 37, 38}
--- TEMPORARY !!
--- local numMotors = 4
--- local motorsPWM     = { -1, -1, -1, -1} -- motorsPWM array
--- local motorsChannel = { 37, 38, 35, 36 }
+local motorsChannel = { 37, 38, 35, 36, 34, 33 }--{ 33, 34, 35, 36, 37, 38}
 
-local sentDisarmMessage     = false   -- to send disarm message only once
-local startTimeModeAuto     = -1      -- will determine the initial time we're actually checking motors to be down
+-- TEMPORARY (for sim only)!!
+-- local numMotors      = 4
+-- local motorsPWM      = { -1, -1, -1, -1} -- motorsPWM array
+-- local motorsChannel  = { 37, 38, 35, 36 }
+
+local timeSinceAuto         = -1    -- will be used to enable or nor some checks
+local finishedTakeoff       = false -- will be used to run code only if we are taking off
+local sentDisarmMessage     = false -- to send disarm message only once
+local startTimeModeAuto     = -1    -- will determine the initial time we're actually checking motors to be down
 local startTimeMotorDown    = -1
+
 -- modes and channel numbers
 local mode -- current aircraft mode
 local COPTER_MODE_STABILIZE = 0
@@ -29,29 +32,32 @@ local COPTER_MODE_SMART_RTL = 21
 -- times
 local WAIT_TIME_MODE_AUTO_MS        = 750  -- wait 750ms after AUTO is enabled before starting to check for problems
 local TIME_DETECTION_THRESHOLD_MS   = 2000 -- (2s) Time frame to run this motor down detection algorithm, aftert WAIT_TIME_MODE_AUTO_MS have passed after mode AUTO (milliseconds)
-local WAIT_TIME_MOTOR_DOWN_MS       = 300  -- (300ms) Time to wait before considering a motor failure, in milliseconds
+local WAIT_TIME_MOTOR_DOWN_MS       = 250  -- (250ms) Time to wait before considering a motor failure, in milliseconds
 
-local roll  -- roll  angle (degrees)
-local pitch -- pitch angle (degrees)
-local yaw   -- yaw   angle (degrees)
+-- angles
+local roll      -- roll  angle (degrees)
+local pitch     -- pitch angle (degrees)
+local yaw       -- yaw   angle (degrees)
 
+-- rates
 local rates     -- aircraft rates
 local roll_rate -- gyro in X axis (rad/s) - roll
 local pitch_rate-- gyro in Y axis (rad/s) - pitch
 local yaw_rate  -- gyro in Z axis (rad/s) - yaw
 
--- Safety check by RPY angles (degrees)
-ROLL_ANGLE_THRESH   = 15
-PITCH_ANGLE_THRESH  = 15
--- YAW_ANGLE_THRESH    = 45
+-- Safety checks
+
+-- Safety check by RP angles (degrees)
+ROLL_ANGLE_THRESH   = 10
+PITCH_ANGLE_THRESH  = 10
 
 -- Safety check by RPY rates (radians/s)
-local ROLL_RATE_THRESH   = 0.90
-local PITCH_RATE_THRESH  = 0.90
-local YAW_RATE_THRESH    = 0.90
+local ROLL_RATE_THRESH   = 0.80
+local PITCH_RATE_THRESH  = 0.80
+local YAW_RATE_THRESH    = 0.80
 
 -- Safety check by motor PWM (microsseconds)
-local MOTOR_PWM_THRESH_MIN = 1400
+local MOTOR_PWM_THRESH_MIN = 1300
 local MOTOR_PWM_THRESH_MAX = 1800
 
 --functions
@@ -66,21 +72,23 @@ local function updateMotorsPWM()
     for i = 1, numMotors, 1 do
         channel     = motorsChannel[i]
         motorsPWM[i]= SRV_Channels:get_output_pwm(channel)
+        -- TEMPORARY !!
         --gcs:send_text(6, string.format("motor %d = %d", i, motorsPWM[i]))
     end
 end
 
+-- get mode, rpy angles and velocities
 local function getAircraftData()
-    mode = vehicle:get_mode() -- get current aircraft mode
+    mode    = vehicle:get_mode() -- get current aircraft mode
 
-    roll  = math.deg(ahrs:get_roll()) -- roll  angle (degrees)
-    pitch = math.deg(ahrs:get_pitch())-- pitch angle (degrees)
-    yaw   = math.deg(ahrs:get_yaw())  -- yaw   angle (degrees)
+    roll        = math.deg(ahrs:get_roll()) -- roll  angle (degrees)
+    pitch       = math.deg(ahrs:get_pitch())-- pitch angle (degrees)
+    yaw         = math.deg(ahrs:get_yaw())  -- yaw   angle (degrees)
 
-    rates = ahrs:get_gyro()
-    roll_rate  = rates:x()    -- gyro in X axis (rad/s) - roll
-    pitch_rate = rates:y()    -- gyro in Y axis (rad/s) - pitch
-    yaw_rate   = rates:z()    -- gyro in Z axis (rad/s) - yaw
+    rates       = ahrs:get_gyro()
+    roll_rate   = rates:x()    -- gyro in X axis (rad/s) - roll
+    pitch_rate  = rates:y()    -- gyro in Y axis (rad/s) - pitch
+    yaw_rate    = rates:z()    -- gyro in Z axis (rad/s) - yaw
     -- gcs:send_text(6, string.format("r,p,y,rr,pr,yr = %.2f,%.2f,%.2f,%.2f,%.2f,%.2f",roll, pitch, yaw, roll_rate, pitch_rate, yaw_rate))
 end
 
@@ -90,6 +98,7 @@ local function RPAnglesOK() -- true if OK
 end
 
 local function RPYRatesOK() -- true if OK
+    gcs:send_text(6, string.format("[motorDownDetection.lua] rpyRates = %.2f,%.2f,%.2f", roll_rate, pitch_rate, yaw_rate))
     return(math.abs(roll_rate) <= ROLL_RATE_THRESH and math.abs(pitch_rate) <= PITCH_RATE_THRESH and math.abs(yaw_rate) <= YAW_RATE_THRESH)
 end
 
@@ -98,7 +107,7 @@ local function isMotorPWMOK(motorNum, motorPWM)
 
     if (motorPWM ~= nil) then
         if (motorPWM >= MOTOR_PWM_THRESH_MIN and motorPWM <= MOTOR_PWM_THRESH_MAX) then
-            -- gcs:send_text(6, string.format("[motorDownDetection.lua] motor %d is OK, PWM = %d", motorNum+1, motorPWM ))    
+            gcs:send_text(6, string.format("[motorDownDetection.lua] motor %d is OK, PWM = %d", motorNum, motorPWM ))    
             return true
         else
             gcs:send_text(6, string.format("[motorDownDetection.lua] motor %d is NOT OK, PWM = %d", motorNum, motorPWM ))
@@ -123,21 +132,32 @@ local function isAnyMotorOutsidePWMRange() -- false if OK
     return result
 end
 
-local function areMotorsPWMOK()
-    if (isAnyMotorOutsidePWMRange()) then
-        -- start couting first moment when motors were considered outside normal range
-        if (startTimeMotorDown <= 0) then
-            startTimeMotorDown = millis()
-        end
+local function handleStartTimeMotorDown()
+    if (startTimeMotorDown == -1) then
+        startTimeMotorDown = millis()
+    end
+end
 
-        if(millis() - startTimeMotorDown >= WAIT_TIME_MOTOR_DOWN_MS) then
-            return false
-        else -- if motors are not outside PWM range for at least WAIT_TIME_MOTOR_DOWN_MS, we are ok 
+local function areMotorsPWMOK()
+    if (timeSinceAuto >= WAIT_TIME_MODE_AUTO_MS)then -- only run after a delay
+        if (isAnyMotorOutsidePWMRange()) then
+            -- start couting first moment when motors were considered outside normal range
+            handleStartTimeMotorDown()
+
+            local timeSinceMotorDown = millis() - startTimeMotorDown
+
+            if(timeSinceMotorDown >= WAIT_TIME_MOTOR_DOWN_MS) then
+                gcs:send_text(6, string.format("[motorDownDetection.lua] Duration of PWM value is >= %d ms!",WAIT_TIME_MOTOR_DOWN_MS))
+                return false
+            else -- if motors are not outside PWM range for at least WAIT_TIME_MOTOR_DOWN_MS, we are ok
+                return true
+            end
+        else -- if no motors outside PWM range, everything is OK
+            startTimeMotorDown = -1 -- restart counting initial motor down time
             return true
         end
-    else -- if no motors outside PWM range, everything is OK
-        startTimeMotorDown = -1 -- restart counting initial motor down time
-        return true
+    else
+        return true -- since we're not analyzing RPM yet, return true so we don't spoil the other aircraft logics
     end
 end
 
@@ -149,51 +169,84 @@ local function motorDownAction()
     arming:disarm() -- disarm first
     if (not sentDisarmMessage) then
         gcs:send_text(6, string.format("[motorDownDetection.lua] Motor Down !"))
-        gcs:send_text(6, string.format("[motorDownDetection.lua] Disarming drone !"))    
+        gcs:send_text(6, string.format("[motorDownDetection.lua] Disarming drone !"))
     end
     sentDisarmMessage = true
 end
 
 local function isAircraftOK()
-    return areMotorsOK() --and RPAnglesOK() 
+    return RPAnglesOK() and RPYRatesOK() and areMotorsOK()
 end
 
-local function shouldCheckMotorDown(timeSinceAuto)
-     -- wait some time before checking for problems, and check them for a period of time only (only during takeoff)
-    return (timeSinceAuto >= WAIT_TIME_MODE_AUTO_MS and timeSinceAuto <= (WAIT_TIME_MODE_AUTO_MS + TIME_DETECTION_THRESHOLD_MS))
+local function checkTakeoffStatus() -- true means "taking off", false otherwise
+
+    if (finishedTakeoff) then
+        gcs:send_text(6, "[motorDownDetector.lua] NOT running code anymore")
+        return false -- if we finished motor dection, return immediately
+    end
+
+     -- consider taking off for a period of time only (first few seconds)
+    local isTakingOff = (timeSinceAuto <= (WAIT_TIME_MODE_AUTO_MS + TIME_DETECTION_THRESHOLD_MS))
+
+    -- if we're not within the time frame to check for motor down
+    if (not isTakingOff) then
+        finishedTakeoff = true -- finished detection
+    end
+
+    return isTakingOff
 end
 
-local function checkMotorDown() -- main loop
+local function timeSinceAutoOverflow()
+    -- to avoid conflicts with any overflow
+    if (timeSinceAuto < 0) then
+        return true
+    else
+        return false
+    end
+end
 
-    if (arming:is_armed()) then
-        if (mode == COPTER_MODE_AUTO) then
-        -- if (mode == COPTER_MODE_GUIDED) then
-            if (startTimeModeAuto <= 0) then
-                gcs:send_text(6, "[motorDownDetector.lua] starting to count startTimeModeAuto")
-                startTimeModeAuto = millis()
-            end
+local function handleTimeSinceAuto()
+    if ( startTimeModeAuto ~= -1 ) then
+        timeSinceAuto = millis() - startTimeModeAuto
+    end
+end
 
-            local timeSinceAuto = millis() - startTimeModeAuto
-            if (shouldCheckMotorDown(timeSinceAuto)) then
-                gcs:send_text(6, "[motorDownDetector.lua] inside WAIT_TIME_MODE_AUTO_MS")
-                if (not isAircraftOK()) then
-                    motorDownAction()
-                end
+local function handleStartTimeModeAuto()
+    if ( startTimeModeAuto == -1 ) then -- will only run in the first time we enter AUTO mode
+        gcs:send_text(6, "[motorDownDetector.lua] starting to count startTimeModeAuto")
+        startTimeModeAuto = millis()
+    end
+end
+
+local function checkAircraftStatus() -- main loop
+
+    if ( arming:is_armed() and mode == COPTER_MODE_AUTO ) then
+
+        handleStartTimeModeAuto()                   -- initializes startTimeModeAuto, if needed
+        handleTimeSinceAuto()                       -- initializes timeSinceAuto, if needed
+        if (timeSinceAutoOverflow()) then return end-- if 'timeSinceAuto' overflows, we return
+
+        if (checkTakeoffStatus()) then  -- true means "taking off", false otherwise
+            --gcs:send_text(6, "[motorDownDetector.lua] inside WAIT_TIME_MODE_AUTO_MS")
+            if (not isAircraftOK()) then
+                motorDownAction()
             end
-        end -- end mode AUTO
+        end
     end -- end if armed()
-    return checkMotorDown
+
+    return checkAircraftStatus
 end
 
 local function main()
 
     --gcs:send_text(6, "--------------------------------")
     updateMotorsPWM()
-    getAircraftData()
-    checkMotorDown()
+    getAircraftData() -- get mode, rpy angles and velocities
+    checkAircraftStatus()
+
     return main, loop_time
 end
 
-gcs:send_text(6, "Started motorDownDetection.lua !")
+gcs:send_text(6, "[motorDownDetection.lua] Started motorDownDetection.lua !")
 
 return main() -- run immediately after the script is launched
