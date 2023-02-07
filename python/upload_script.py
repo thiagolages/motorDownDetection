f4f703f8d14834d68ad416b574d80b4c7dc25b73
Thiago Lages <thiagolagesrocha@gmail.com>
2023-02-07 12:22:57 -0300

    Organize folder structure.

--- python/upload_script.py
+++ python/upload_script.py
@@ -2,8 +2,8 @@ import os
 
 # address to connect to your aircraft. Either locally (SITL) or real aircraft via wifi (something like 192.168.0.X,115200)
 # Make sure to use the baud rate, separated by comma, in 'master_address', if connecting to your real aircraft (and not locally)
-#master_address              = "\"127.0.0.1:14660\"" 
-master_address              = "/dev/serial/by-id/usb-Hex_ProfiCNC_CubeOrange_27003F000E51313132383631-if00,115200"
+master_address              = "\"127.0.0.1:14880\"" 
+#master_address              = "/dev/serial/by-id/usb-Hex_ProfiCNC_CubeOrange_27003F000E51313132383631-if00,115200"
 local_scripts_folder        = os.getcwd() + "/"
 ardupilot_scripts_folder    = "APM/scripts/"
 lua_script_name             = "motorDownDetection.lua"
